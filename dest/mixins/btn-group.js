9369e56ef94b1769fa5a8e7b722785adee9a0153
tomchentw <developer@tomchentw.com>
2013-10-03 11:15:24 +0800

    Add dest to gitignore

--- dest/mixins/btn-group.js
+++
@@ -1,4 +0,0 @@
-this.ButtonGroupCtrl = ['$log', '$scope', 'FireSync'].concat(function($log, $scope, FireSync){
-  $scope.states = new FireSync().get(FirebaseURL + "/button-states").sync();
-  $log.log('ButtonGroupCtrl', $scope);
-});
--- dest/mixins/btn-toolbar.js
+++
@@ -1,6 +0,0 @@
-this.ButtonToolbarCtrl = ['$log', '$scope', 'FireSync'].concat(function($log, $scope, FireSync){
-  $scope.states = new FireSync().get(FirebaseURL + "/button-states", {
-    toCollection: true
-  }).sync();
-  $log.log('ButtonToolbarCtrl', $scope);
-});
