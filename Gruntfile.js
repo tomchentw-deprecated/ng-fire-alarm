b349ef710ed2cc028dfb503ccf31052200ca618e
tomchentw <developer@tomchentw.com>
2013-10-22 19:50:12 +0800

    Fix banner layout

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,11 +27,12 @@ module.exports = function(grunt) {
       tmp:    'tmp/',
       vendor: 'vendor/'
     },
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+    banner: '' +
+      '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
       '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
       '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> [<%= pkg.author.name %>](<%= pkg.author.url %>);\n' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+      ' * Copyright (c) <%= grunt.template.today("yyyy") %> [<%= pkg.author.name %>](<%= pkg.author.url %>);\n' +
+      ' * Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
     // Task configuration.
     'curl-dir': {
       scripts: {
