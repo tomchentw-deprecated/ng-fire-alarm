039ced1b74b573d8d6b79fe4619d269030b9ef57
tomchentw <developer@tomchentw.com>
2014-01-17 17:13:54 +0800

    fix(rubygems): rename to correct module name

--- lib/ng/fire/alarm.rb
+++ lib/ng/fire/alarm.rb
@@ -1,9 +1,11 @@
 require "ng/fire/alarm/package"
 
-module Firebase
-  module Q
-    class Engine < ::Rails::Engine
-    # http://prioritized.net/blog/gemify-assets-for-rails/
+module Ng
+  module Fire
+    module Alarm
+      class Engine < ::Rails::Engine
+      # http://prioritized.net/blog/gemify-assets-for-rails/
+      end
     end
   end
 end
--- lib/ng/fire/alarm/package.rb
+++ lib/ng/fire/alarm/package.rb
@@ -1,11 +1,13 @@
 require 'json'
 
-module Firebase
-  module Q
-    JSON.parse(File.read(
-      File.expand_path('../../../../package.json', __FILE__)
-    )).each do |key, value|
-      const_set(key.upcase, value)
+module Ng
+  module Fire
+    module Alarm
+      JSON.parse(File.read(
+        File.expand_path('../../../../../package.json', __FILE__)
+      )).each do |key, value|
+        const_set(key.upcase, value)
+      end
     end
   end
 end
